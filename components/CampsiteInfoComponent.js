9932a73bc3ec94774e8f2b4906d2f679df55e16b
Luis Juarros <juarrosl0806@gmail.com>
2020-04-23 01:00:34 -0700

    Components Part2

---
+++ components/CampsiteInfoComponent.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import { Card } from 'react-native-elements';
+
+
+//destructuring campsite so we can use array object as variables {{id, name}}
+function RenderCampsite({campsite}) {
+        if(campsite) {
+            return (
+                <Card
+                    featuredTitle={campsite.name}
+                    image={require('./images/react-lake.jpg')}>
+                    <Text style={{margin:10}}>
+                        {campsite.description}
+                    </Text>
+                </Card>
+            );
+        }
+        //falsy
+        return <View />;
+}
+
+
+function CampsiteInfo(props) {
+   return <RenderCampsite campsite={props.campsite} />;
+}
+
+export default CampsiteInfo;
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,17 +1,38 @@
 import React, { Component } from 'react';
 import Directory from './DirectoryComponent';
+import CampsiteInfo from './CampsiteInfoComponent';
+import { View } from 'react-native';
 import { CAMPSITES } from '../shared/campsites';
+
 
 class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
-          campsites: CAMPSITES
+          campsites: CAMPSITES,
+          selectedCampsite: null
         };
     }
 
+    //pass state when clicked or selected
+    onCampsiteSelect(campsiteId) {
+        this.setState({selectedCampsite: campsiteId});
+    }
+
+    //onPress is passed to directory component with the campsites Id's
     render() {
-        return <Directory campsites={this.state.campsites} />;
+        return (
+            <View style={{flex: 1}}>
+                <Directory 
+                    campsites={this.state.campsites} 
+                    onPress={campsiteId => this.onCampsiteSelect(campsiteId)} 
+                />
+                <CampsiteInfo 
+                    campsite={this.state.campsites.filter(campsite => 
+                    campsite.id === this.state.selectedCampsite)[0]} 
+                />
+            </View>
+        );
     }
 }
 
