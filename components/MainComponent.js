360ad8893de472d300b17973437eda2b474b71a3
Luis Juarros <juarrosl0806@gmail.com>
2020-04-29 00:03:09 -0700

    Drawer Navigator Icons

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -4,14 +4,30 @@ import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import About from './AboutComponent';
 import Contact from './ContactComponent';
-import { View, Platform } from 'react-native';
-import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
+import { View, Platform, StyleSheet, Text, ScrollView, Image } from 'react-native';
+import { createStackNavigator, createDrawerNavigator, DrawerItems } from 'react-navigation';
+import { Icon } from 'react-native-elements';
+import SafeAreaView from 'react-native-safe-area-view';
+
 
 //createStack. has one required argument. What components available:
 //are each screen connected to one another? why not put home here? Its a history tracker
 const DirectoryNavigator = createStackNavigator(
     {
-        Directory: { screen: Directory },
+        //specified style for one screen
+        Directory: { 
+            screen: Directory,
+            navigationOptions: ({navigation}) => ({
+                headerLeft: <Icon
+                    name='list'
+                    type='font-awesome'
+                    iconStyle={styles.stackIcon}
+                    onPress={() => navigation.toggleDrawer()}
+                />
+            })
+        }
+,
+
         CampsiteInfo: { screen: CampsiteInfo }
     },
     {
@@ -35,15 +51,22 @@ const HomeNavigator = createStackNavigator(
     },
 
     {
-        navigationOptions: {
+        navigationOptions: ({navigation}) =>  ({
             headerStyle: {
                 backgroundColor: '#5637DD'
             },
             headerTintColor: '#fff',
             headerTitleStyle: {
                 color: '#fff'
-            }
-        }
+            },
+            //made an icon for each page to toggle drawer
+            headerLeft: <Icon
+                name='home'
+                type='font-awesome'
+                iconStyle={styles.stackIcon}
+                onPress={() => navigation.toggleDrawer()}
+        />
+        })
     }
 );
 
@@ -54,15 +77,21 @@ const AboutNavigator = createStackNavigator(
     },
 
     {
-        navigationOptions: {
+        navigationOptions: ({navigation}) =>  ({
             headerStyle: {
                 backgroundColor: '#5637DD'
             },
             headerTintColor: '#fff',
             headerTitleStyle: {
                 color: '#fff'
-            }
-        }
+            },
+            headerLeft: <Icon
+                name='info-circle'
+                type='font-awesome'
+                iconStyle={styles.stackIcon}
+                onPress={() => navigation.toggleDrawer()}
+        />
+        })
     }
 );
 
@@ -72,16 +101,46 @@ const ContactNavigator = createStackNavigator(
     },
 
     {
-        navigationOptions: {
+        navigationOptions: ({navigation}) =>  ({
             headerStyle: {
                 backgroundColor: '#5637DD'
             },
             headerTintColor: '#fff',
             headerTitleStyle: {
                 color: '#fff'
-            }
-        }
+            },
+            headerLeft: <Icon
+                name='address-card'
+                type='font-awesome'
+                iconStyle={styles.stackIcon}
+                onPress={() => navigation.toggleDrawer()}
+        />
+        })
     }
+);
+
+
+
+
+
+//safe are for iphone X physical design and since it is a custom drawer
+//all this is for the side icons and Company Header on drawer apparently
+const CustomDrawerContentComponent = props => (
+    <ScrollView> 
+        <SafeAreaView 
+            style={styles.container}
+            forceInset={{top: 'always', horizontal: 'never'}}>
+            <View style={styles.drawerHeader}>
+                <View style={{flex: 1}}>
+                    <Image source={require('./images/logo.png')} style={styles.drawerImage} />
+                </View>
+                <View style={{flex: 2}}>
+                    <Text style={styles.drawerHeaderText}>NuCamp</Text>
+                </View>
+            </View>
+            <DrawerItems {...props} />
+        </SafeAreaView>
+    </ScrollView>
 );
 
 
@@ -90,16 +149,71 @@ const ContactNavigator = createStackNavigator(
 //this one is different navigator. So far we have both stacks in here connected toa main hold all navigator
 const MainNavigator = createDrawerNavigator(
     {
-        Home: { screen: HomeNavigator },
-        Directory: { screen: DirectoryNavigator},
-        About: { screen: AboutNavigator },
-        Contact: { screen: ContactNavigator }
+        Home: {
+            //tint color is default selected and unselected
+            screen: HomeNavigator,
+            navigationOptions: {
+                drawerIcon: ({tintColor}) => (
+                    <Icon
+                        name='home'
+                        type='font-awesome'
+                        size={24}
+                        color={tintColor}
+                    />
+                )
+            }
+        },
+        Directory: { 
+            screen: HomeNavigator,
+            navigationOptions: {
+                drawerIcon: ({tintColor}) => (
+                    <Icon
+                        name='list'
+                        type='font-awesome'
+                        size={24}
+                        color={tintColor}
+                    />
+                )
+            }
+        },
+        About: {
+            screen: HomeNavigator,
+            navigationOptions: {
+                drawerLabel: 'About Us',
+                drawerIcon: ({tintColor}) => (
+                    <Icon
+                        name='info-circle'
+                        type='font-awesome'
+                        size={24}
+                        color={tintColor}
+                    />
+                )
+            }
+        },
+        Contact: {
+            screen: HomeNavigator,
+            navigationOptions: {
+                drawerLabel: 'Contact Us',
+                drawerIcon: ({tintColor}) => (
+                    <Icon
+                        name='address-card'
+                        type='font-awesome'
+                        size={24}
+                        color={tintColor}
+                    />
+                )
+            }
+        }
+
     },
-    {             
-        drawerBackgroundColor: '#CEC8FF'
+
+    {           
+        drawerBackgroundColor: '#CEC8FF',
+        //here we pass all this prop items into the custom one to render at the end.
+        contentComponent: CustomDrawerContentComponent
     }
 
-)
+);
 
 
 class Main extends Component {
@@ -118,5 +232,37 @@ class Main extends Component {
         );
     }
 }
+
+
+
+//stylesheet for the stack navigators
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    drawerHeader: {
+        backgroundColor: '#5637DD',
+        height: 140,
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        flexDirection: 'row'
+    },
+    drawerHeaderText: {
+        color: '#fff',
+        fontSize: 24,
+        fontWeight: 'bold'
+    },
+    drawerImage: {
+        margin: 10,
+        height: 60,
+        width: 60
+    },
+    stackIcon: {
+        marginLeft: 10,
+        color: '#fff',
+        fontSize: 24
+    }
+});
 
 export default Main;
